f5549ff7b0e17dd7cab0741f9d3462292f462a71
Kirillmlk <kostakov.kiirill@yandex.by>
2024-08-25 17:51:19 +0300

    header is done

--- sneakers-vue/src/App.vue
+++ sneakers-vue/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 import Hello from "@/components/Hello.vue";
+import Header from "@/components/Header.vue";
 
 
 </script>
 
 <template>
- <Hello></Hello>
+    <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+        <Header></Header>
+        <h1>fgdgdfgfdg</h1>
+    </div>
 </template>
 
 <style scoped>
